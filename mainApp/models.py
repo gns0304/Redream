2c8a4c4b1361d258878f25119bf5ec43e0883f53
Yubin_Park on Window <itqkrdbqls1001@naver.com>
2020-07-25 23:09:55 +0900

    박유빈 중간점검

--- mainApp/models.py
+++ mainApp/models.py
@@ -7,7 +7,7 @@ class HelpPost(models.Model):
     finished = models.BooleanField(default=False, null=False, blank=False, verbose_name="마감 여부")
     description = models.TextField(null=False, blank=False, verbose_name="특이사항")
 
-class RecieverComment(models.Model):
+class ReceiverComment(models.Model):
     author = models.ForeignKey('mainApp.Receiver', related_name='receiverCommentAuthor', null=False, blank=False, on_delete=models.CASCADE, verbose_name="댓글을 남긴 수혈자")
     pub_date = models.DateTimeField(null=False, blank=False, verbose_name="댓글 작성일")
     body = models.TextField(null=False, blank=False, verbose_name="댓글 내용")
