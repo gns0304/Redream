90151e9efd7f66208dbcbe7b092f6c579f867a50
RYOU Chanmi <ryoucm0417@gmail.com>
2020-07-26 00:23:52 +0900

    account.view 중간저장

--- account/views.py
+++ account/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render, redirect
-from .models import Account
+from .forms import PostForm, SigninForm, UserForm
 
 # Create your views here.
+#회원가입 
 def signUp(request):
     return render(request, 'signup.html')
+    
 
 def signUp_complete(request) :
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid() :
+            if user = donator
+            new_user = user.objects.create_user(nickname=form.cleaned_Data['nickname'],
+            number=form.cleaned_data['number'], bloodtype=form.cleaned_data['bloodtype'],
+            location=form.cleaned_data['location'])
+    form = PostForm(request.POST)
+    form.save()
     return render(request, 'signupcomplete.html')
 
 def signUp_check(request) :
     return redirect('index.html')
+
+#로그인
+def signIn(request) :
+    return render(request, 'signin.html')
+
+def signIn_check(request) :
+    return redirect('index.html')
+
+#글쓰기
+def write(request) :
+    return render('postcreate.html')
+
+def write_page(request) :
+
+    return render(request, 'list.html')
+
