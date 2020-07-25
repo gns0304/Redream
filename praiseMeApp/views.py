2d706dc90b68a2ea9778214cc95ca0dd40cf353c
juyoung <wndud5570@naver.com>
2020-07-26 01:25:33 +0900

    살려주세요

--- praiseMeApp/views.py
+++ praiseMeApp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import PraiseMePost, DonatorPraiseMeComment, ReceiverPraiseMeComment
+from .form import PostingForm
+
