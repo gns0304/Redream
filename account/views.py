670f4041144b452d8c247ef4a717bb893c058af5
RYOU Chanmi <ryoucm0417@gmail.com>
2020-07-25 23:08:17 +0900

    유찬미 중간저장

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Account
 
 # Create your views here.
+def signUp(request):
+    return render(request, 'signup.html')
+
+def signUp_complete(request) :
+    return render(request, 'signupcomplete.html')
+
+def signUp_check(request) :
+    return redirect('index.html')
